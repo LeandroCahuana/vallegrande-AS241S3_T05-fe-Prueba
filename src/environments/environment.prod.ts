26e3bd9e7e5c00e9cc3ba17d9ab6cc9f0301861a
LeandroCahuana <leandro.cahuana@vallegrande.edu.pe>
2025-10-29 09:36:36 +0000

    feat(app-web): App-web Cloud

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://shadowy-crypt-7vr5pggvqp5xcrj4r-8085.app.github.dev',
+  storageUrl: 'https://storage.googleapis.com/nirespaimage/'
+};
